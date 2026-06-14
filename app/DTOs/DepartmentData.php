b5ccf7d1390f6a1403bbd7f486ae1549ecbb5e1c
Manishraii-web <raimanish489@gmail.com>
2026-06-14 13:44:39 +0545

    mdepartment data

--- app/DTOs/DepartmentData.php
+++ app/DTOs/DepartmentData.php
@@ -6,11 +6,30 @@ use App\Enums\DepartmentStatus;
 
 class DepartmentData {
     public function __construct(
-        public readonly int $tenant_id,
+        public readonly int $tenantId,
         public readonly string $name,
         public readonly string $code,
         public readonly ?string $description = null,
         public readonly DepartmentStatus $status = DepartmentStatus::Active,) { }
 
-        public static function fromArray(array $data):self
+        public static function fromArray(array $data):self  {
+            return new self(
+                tenantId: $data['tenant_id'],
+                name: $data['name'],
+                code : $data['code'],
+                description: $data['description'] ??null,
+                status: isset($data['status'])
+                  ? DepartmentStatus::from($data['status']) : DepartmentStatus::Active,
+                );
+        }
+        public function toArray(): array
+        {
+            return [
+            'tenant_id' => $this->tenantId,
+            'name' => $this->name,
+            'code' =>$this->code,
+            'description' => $this->description,
+            'status' => $this->status->value,
+            ];
+        }
 }
